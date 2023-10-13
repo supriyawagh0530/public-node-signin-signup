da3db5f18e3078d6436e3ac88df6af3ab715771b
supriyawagh0530 <supriyawagh0530@gmail.com>
2023-10-13 15:07:56 +0530

    added node api for signin signup

---
+++ app/models/role.model.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+mongoose.set('strictQuery', false);
+
+const Role = mongoose.model(
+  "Role",
+  new mongoose.Schema({
+    name: String
+  })
+);
+
+module.exports = Role;
